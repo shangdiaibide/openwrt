c38a7f2c2a68d553557e8f53c82b81a222b47278
Lighacker <597223649@qq.com>
2020-02-11 16:52:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+git clone https://github.com/kuoruan/luci-app-frpc.git package/frpc
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
