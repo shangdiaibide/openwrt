ad1214d99ae847404ae2320e6e162e00b65203bc
Lighacker <597223649@qq.com>
2020-02-11 11:30:26 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
+git clone https://github.com/hong0980/luci-app-passwall.git package/passwall
+./scripts/feeds update -a
+./scripts/feeds install -a
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
